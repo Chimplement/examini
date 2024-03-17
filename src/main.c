046d8325ee08ffb21185fe1d5d949243aeac8822
Tycho <tychosteur@gmail.com>
2024-03-17 20:44:20 +0100

    cleared comments

--- src/main.c
+++ src/main.c
@@ -45,9 +45,7 @@ int main(int argc, char* argv[])
 	if (waitpid(tracee_pid, NULL, 0) == -1)
 		exit_error(errno);
 
-	// struct user_regs_struct		old_regs;
 	struct user_regs_struct			regs;
-	// bool							bold = false;
 	long							ip_long;
 	ZyanU8							ip_buffer[sizeof(long)];
 	ZydisDisassembledInstruction	instruction; 
@@ -62,40 +60,17 @@ int main(int argc, char* argv[])
 		if (ptrace(PTRACE_GETREGS, tracee_pid, 0, &regs) == -1)
 			break;
 
-		// data = ptrace(PTRACE_PEEKDATA, tracee_pid, 0, regs.rsp);
-		// printf("rbp: %llx, rsp: %llx\n", regs.rbp, regs.rsp);
-		// printf("size of stack: %lli\n", regs.rbp - regs.rsp);
-		// write(1, "stack top: ", 11);
-		// write(1, &data, 8);
-		// write(1, "\n", 1);
-		// printf(BOLD" -- STEP: --\n"RESET_BOLD);
 		errno = 0;
 		ip_long = ptrace(PTRACE_PEEKTEXT, tracee_pid, regs.rip, 0);
 		if (errno != 0)
 			break;
 		memcpy(ip_buffer, &ip_long, sizeof(long));
 		
-		
 		if (ZYAN_SUCCESS(ZydisDisassembleIntel(ZYDIS_MACHINE_MODE_LONG_64, 0, 
 											ip_buffer, sizeof(ip_buffer), &instruction)))
 		{
 			printf("%s\n", instruction.text); 
 		}
-		// if (memcmp(&regs, &old_regs, sizeof(regs)))
-		// {
-		// 	if (bold)
-		// 		printf(BOLD);
-		// 	printf("rax: %-16llx\t", regs.rax);
-		// 	printf("rdi: %-16llx\t", regs.rdi);
-		// 	printf("rsi: %-16llx\t", regs.rsi);
-		// 	printf("rdx: %-16llx\t", regs.rdx);
-		// 	printf("rcx: %-16llx\t", regs.rcx);
-		// 	if (bold)
-		// 		printf(RESET_BOLD);
-		// 	printf("\n");
-		// 	bold = !bold;
-		// }
-		// old_regs = regs;
 	}
 	if (ptrace(PTRACE_DETACH, tracee_pid, 0, 0) == -1)
 		exit_error(errno);
