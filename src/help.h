89f7a6a73d9300713fe3d765087b421e41b914ad
Tycho <tychosteur@gmail.com>
2024-03-17 21:48:42 +0100

    moved help function to separate .c file

---
+++ src/help.h
@@ -0,0 +1,7 @@
+#ifndef HELP_H
+# define HELP_H
+
+void help(char* program_name);
+void expanded_help(char* program_name);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -14,27 +14,7 @@
 #include "Zydis/Disassembler.h"
 
 #include "ansi.h"
-
-void help(char* program_name)
-{
-	printf("Expected:\n");
-	printf("%s "UNDERLINE"COMMAND"RESET_UNDERLINE" ["UNDERLINE"ARGS"RESET_UNDERLINE"...]\n", program_name);
-	printf("%s -p "UNDERLINE"PID"RESET_UNDERLINE"\n", program_name);
-	printf("%s -h\n", program_name);
-}
-
-void expanded_help(char* program_name)
-{
-	printf("%s "UNDERLINE"COMMAND"RESET_UNDERLINE" ["UNDERLINE"ARGS"RESET_UNDERLINE"...]\n", program_name);
-	printf("\tStarts "UNDERLINE"COMMAND"RESET_UNDERLINE" in a child process, showing every instruction it executes.\n");
-	printf("\n");
-	printf("%s -p "UNDERLINE"PID"RESET_UNDERLINE"\n", program_name);
-	printf("\tStarts observing the process identified by "UNDERLINE"PID"RESET_UNDERLINE", showing every instruction it executes.\n");
-	printf("\n");
-	printf("%s -h\n", program_name);
-	printf("\tShows this menu.\n");
-	printf("\n");
-}
+#include "help.h"
 
 void exit_error(int error_code)
 {
