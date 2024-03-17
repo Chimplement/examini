49f9153c65a1ecd1134a6256c00dbf664205112f
Tycho <tychosteur@gmail.com>
2024-03-17 21:45:42 +0100

    added an expanded help menu with descriptions for each option

--- src/main.c
+++ src/main.c
@@ -20,7 +20,20 @@ void help(char* program_name)
 	printf("Expected:\n");
 	printf("%s "UNDERLINE"COMMAND"RESET_UNDERLINE" ["UNDERLINE"ARGS"RESET_UNDERLINE"...]\n", program_name);
 	printf("%s -p "UNDERLINE"PID"RESET_UNDERLINE"\n", program_name);
-	printf("%s -h");
+	printf("%s -h\n", program_name);
+}
+
+void expanded_help(char* program_name)
+{
+	printf("%s "UNDERLINE"COMMAND"RESET_UNDERLINE" ["UNDERLINE"ARGS"RESET_UNDERLINE"...]\n", program_name);
+	printf("\tStarts "UNDERLINE"COMMAND"RESET_UNDERLINE" in a child process, showing every instruction it executes.\n");
+	printf("\n");
+	printf("%s -p "UNDERLINE"PID"RESET_UNDERLINE"\n", program_name);
+	printf("\tStarts observing the process identified by "UNDERLINE"PID"RESET_UNDERLINE", showing every instruction it executes.\n");
+	printf("\n");
+	printf("%s -h\n", program_name);
+	printf("\tShows this menu.\n");
+	printf("\n");
 }
 
 void exit_error(int error_code)
@@ -38,6 +51,11 @@ int main(int argc, char* argv[], char* envp[])
 	}
 
 	int tracee_pid;
+	if (!strcmp(argv[1], "-h"))
+	{
+		expanded_help(argv[0]);
+		return (0);
+	}
 	if (!strcmp(argv[1], "-p"))
 	{
 		if (argc != 3)
